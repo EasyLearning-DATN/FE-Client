cdffa9e6707b63b139f7e9dd8661f6fc840d501e
AnhDT <anhdt@anhcoder.net>
2024-02-26 09:05:48 +0700

    Add create-lesson and list-lesson components

--- src/app/components/lesson/create-lesson/create-lesson.component.ts
+++ src/app/components/lesson/create-lesson/create-lesson.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-lesson',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-lesson.component.css']
 })
 export class CreateLessonComponent {
+  createLessonF: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createLessonF = this.fb.group({
+      title: [''],
+      description: [''],
+      image: [''],
+      questions: this.fb.array([
+        this.createQuestionFormGroup()
+      ])
+    });
+  }
+
+  createQuestionFormGroup(): FormGroup {
+    return this.fb.group({
+      question: [''],
+      answerA: [''],
+      answerB: [''],
+      answerC: [''],
+      correctAnswer: ['']
+    });
+  }
+
+  get questionForms() {
+    return this.createLessonF.get('questions') as FormArray;
+  }
+
+  addQuestion(): void {
+    this.questionForms.push(this.createQuestionFormGroup());
+  }
+
+  onCreateLesson(): void {
+    // thông báo tạo bài học thành công
+    console.log(this.createLessonF.value);
+  }
+
+  onFileSelected() {
+    
+}
 
 }
