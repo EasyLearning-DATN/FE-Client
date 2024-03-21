bdb868b1dc2ad2b57e05d94717856af9298cdf84
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2024-03-21 18:52:50 +0700

    Update: update auth interceptor

--- src/app/services/report/report.service.ts
+++ src/app/services/report/report.service.ts
@@ -1,24 +1,25 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { image } from 'ngx-bootstrap-icons';
-import { Observable, switchMap } from 'rxjs';
-import { ReportDTO } from 'src/app/DTOS/report/report.dto';
-import { environment } from 'src/environments/environments';
-import { UploadImageService } from '../shared/upload/upload-image.service';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {ReportDTO} from 'src/app/DTOS/report/report.dto';
+import {environment} from 'src/environments/environments';
+import {UploadImageService} from '../shared/upload/upload-image.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportService {
 
 
   private apiReport = environment.API_URL + environment.API_MEMBER + environment.VERSION_1 + environment.API_REPORT;
+
   constructor(
     private http: HttpClient,
     private router: Router,
     private imageSrv: UploadImageService,
-  ) { }
+  ) {
+  }
 
   sendReport(reportDTO: ReportDTO, imageReport: any): Observable<any> {
     const formData = new FormData();
@@ -27,12 +28,7 @@ export class ReportService {
     formData.append('type', reportDTO.type);
     formData.append('image', imageReport);
     console.log(imageReport);
-    const token = localStorage.getItem('token') || 'null';
-    return this.http.post(this.apiReport, formData, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+    return this.http.post(this.apiReport, formData);
   }
-  
+
 }
