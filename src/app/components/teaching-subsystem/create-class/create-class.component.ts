ecd4a74e8534139088709b90bfd9a19999e565f3
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2024-04-12 22:12:48 +0700

    Update: reformat teaching-subsystem

--- src/app/components/teaching-subsystem/create-class/create-class.component.ts
+++ src/app/components/teaching-subsystem/create-class/create-class.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-create-class',
   templateUrl: './create-class.component.html',
-  styleUrls: ['./create-class.component.css']
+  styleUrls: ['./create-class.component.css'],
 })
 export class CreateClassComponent {
 
