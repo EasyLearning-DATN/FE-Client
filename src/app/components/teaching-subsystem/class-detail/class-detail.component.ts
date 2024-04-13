11f623d2e155159fa9f45911cdcbb6e4c7094d85
AnhDT <anhdt@anhcoder.net>
2024-04-13 15:45:11 +0700

    Update routing paths and API URLs to match the new naming conventions and endpoints

--- src/app/components/teaching-subsystem/class-detail/class-detail.component.ts
+++ src/app/components/teaching-subsystem/class-detail/class-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ClassroomService } from 'src/app/services/classroom/classroom.service';
+import { SharedService } from 'src/app/services/shared/shared.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-class-detail',
@@ -6,5 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./class-detail.component.css']
 })
 export class ClassDetailComponent {
+  classroom: any;
+  studentEmail: string = '';
+  @ViewChild('modal') modal: any;
+
+  constructor(
+    private classroomService: ClassroomService,
+    private route: ActivatedRoute,
+    private modalService: NgbModal
+  ) { }
+
+  ngOnInit(): void {
+    this.getClassroom();
+  }
+
+  getClassroom() {
+    const id = this.route.snapshot.paramMap.get('id') as string;
+    this.classroomService.getOneClassroom(id).subscribe((data: any) => {
+      this.classroom = data.data;
+    });
+  }
+
+  // invite students to join the class
+  inviteStudents() {
+    const emailStudent: string[] = this.studentEmail.split('\n').map(email => email.trim());
+    Swal.fire({
+      title: 'Loading',
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+      willClose: () => {
+        Swal.hideLoading();
+      }
+    });
+    console.log(this.classroom.id);
+    this.classroomService.inviteStudentToClassroom(this.classroom.id, emailStudent).subscribe((data: any) => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Thành công',
+        text: data.data,
+      });
+    });
+  }
+
+  openModal() {
+      this.modalService.open(this.modal);
+  }
 
 }
--- src/app/services/classroom/classroom.service.ts
+++ src/app/services/classroom/classroom.service.ts
@@ -10,12 +10,35 @@ import {SharedService} from '../shared/shared.service';
 export class ClassroomService {
 
   private apiCreateClassroom = environment.API_URL + environment.API_MEMBER + environment.VERSION_1 + environment.API_CLASSROOM;
-
+  private apiGetAllClassroom = environment.API_URL + environment.API_MEMBER + environment.VERSION_1 + environment.API_CLASSROOM;
+  private apiGetOneClassroom = environment.API_URL + environment.API_MEMBER + environment.VERSION_1 + environment.API_CLASSROOM;
+  private apiInviteStudent = environment.API_URL + environment.API_MEMBER + environment.VERSION_1 + environment.API_CLASSROOM + environment.API_INVITE_STUDENT;
+  private apiJoinClassroom = environment.API_URL + environment.API_MEMBER + environment.VERSION_1 + environment.API_CLASSROOM + environment.API_JOIN_CLASSROOM;
   constructor(private http: HttpClient, private sharedService: SharedService) {
   }
 
   createClassroom(classroomDTO: ClassroomDTO) {
     return this.http.post(this.apiCreateClassroom, classroomDTO);
+  }
+
+  getAllClassroom() {
+    return this.http.get(this.apiGetAllClassroom);
+  }
+
+  getOneClassroom(id: string) {
+    return this.http.get(this.apiGetOneClassroom + '/' + id);
+  }
+
+  inviteStudentToClassroom( classRoomId: string, emails: string[]) {
+    const body = {
+      classRoomId,
+      emails
+    };
+    return this.http.put(this.apiInviteStudent, body);
+  }
+
+  joinClassroom(token: string) {
+    return this.http.get(this.apiJoinClassroom + '?token=' + token);
   }
 
 }
