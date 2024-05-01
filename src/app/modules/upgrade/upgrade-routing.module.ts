3ee6092ef5fb04a89d40081e68001c5b797a0b39
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2024-05-01 22:33:42 +0700

    Update: update some bugs (with security)

--- src/app/modules/upgrade/upgrade-routing.module.ts
+++ src/app/modules/upgrade/upgrade-routing.module.ts
@@ -4,9 +4,11 @@ import {InvoiceComponent} from '../../components/upgrade/invoice/invoice.compone
 import {PaymentSuccessComponent} from '../../components/upgrade/success/success.component';
 import {UpgradeComponent} from '../../components/upgrade/upgrade/upgrade.component';
 import {authCanActivateGuard} from '../../guards/auth.can-activate.guard';
+import {authResolver} from '../../resolver/auth.resolver';
+import {roleResolver} from '../../resolver/role.resolver';
 
 const routes: Routes = [
-  {path: '', component: UpgradeComponent, canActivate: [authCanActivateGuard]},
+  {path: '', component: UpgradeComponent, canActivate: [authCanActivateGuard], resolve: [authResolver, roleResolver]},
   {path: 'payment-success', component: PaymentSuccessComponent, canActivate: [authCanActivateGuard]},
   {path: 'invoice', component: InvoiceComponent, canActivate: [authCanActivateGuard]},
 ];
